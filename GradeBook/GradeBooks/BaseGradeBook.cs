2d91a736c29084a19e808272f850623f1e548d3a
palinavasilyeva <palina.vasilyeva@microsoft.wsei.edu.pl>
2025-03-01 18:11:44 +0100

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -23,7 +23,43 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+
+            var sortedGrades = Students.OrderByDescending(s => s.AverageGrade).ToList();
+
+            int studentsInEachPercentile = (int)Math.Ceiling(Students.Count / 5.0);
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (sortedGrades[i * studentsInEachPercentile].AverageGrade <= averageGrade)
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            return 'A';
+                        case 1:
+                            return 'B';
+                        case 2:
+                            return 'C';
+                        case 3:
+                            return 'D'; 
+                        default:
+                            return 'F'; 
+                    }
+                }
+            }
+            return 'F';
+        }
+
     }
+
+
     public class BaseGradeBook
     {
         public GradeBookType Type { get; set; }
